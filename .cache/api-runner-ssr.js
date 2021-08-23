1e6fb055fd4cf1c2e5fc55a546955f1349869385
MatthewBryanCC <thepianomanz1011@gmail.com>
2021-08-24 03:04:13 +0800

    Carousel Update
    The Carousel.js now allows you to add images which are native to '/img/[Image_Name].[File_Extension]/'
    
    Any amount of images should work.

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,6 +1,6 @@
 var plugins = [{
       name: 'gatsby-plugin-google-analytics',
-      plugin: require('D:/EndProject/Quick-Cut-Gatsby-React/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
+      plugin: require('C:/Users/AuraG/Desktop/Websites/Nelgara-Gatsby/nelgaraptyltd/Quick-Cut-Gatsby-React/node_modules/gatsby-plugin-google-analytics/gatsby-ssr'),
       options: {"plugins":[],"trackingId":"UA-205628146-1","head":false,"anonymize":true,"respectDNT":true,"exclude":["/preview/**","/do-not-track/me/too/"],"pageTransitionDelay":0,"optimizeId":"UA-205628146-1","experimentId":"UA-205628146-1","variationId":"UA-205628146-1","defer":false,"sampleRate":5,"siteSpeedSampleRate":10,"cookieDomain":"https://www.quickcutwa.com.au/","enableWebVitalsTracking":true},
     }]
 /* global plugins */
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,9 +3,22 @@ import "../styles/carousel.css"
 
 export default function Carousel() {
 
+    /*
+        ALL CAROUSEL IMAGES GO BELOW
+    */
 
+    var Images = [
+        '/img/Before.png',
+        '/img/After.png',
+        '/img/Before.png'
+    ]
+
+    
     var CarouselIndex = 1; //Carousel First position
-    var MaxPosition = 3; //Carousel Final position
+    var MaxPosition = Images.length; //Carousel Final position
+
+
+    var firstImage = Images[0];
 
     //Move the carousel forward once.
     function Next() {
@@ -22,7 +35,6 @@ export default function Carousel() {
     //Move the carousel backwards once.
     function Previous() {
         CarouselIndex = (CarouselIndex-1 <= 0) ? (MaxPosition) : (CarouselIndex-1);
-        console.log("Index:" + CarouselIndex);
         UpdateCount(CarouselIndex);
 
         var contentElement = document.getElementById("BA_Content");
@@ -39,6 +51,8 @@ export default function Carousel() {
             contentElement.classList.add("animated");
             contentElement.classList.add("fadeIn"+direction);
 
+            SetNewImage(CarouselIndex-1);
+
             setTimeout(()=>{
                 contentElement.classList.remove("animated");
                 contentElement.classList.remove("fadeInLeft");
@@ -47,10 +61,15 @@ export default function Carousel() {
         }, 700);
     }
 
+    function SetNewImage(imageIndex) {
+        var imageElement = document.getElementById("CarouselContentImage");
+        imageElement.style.backgroundImage = "url("+ Images[imageIndex] +")";
+    }
+
     //Update the carousel progress display div.
     function UpdateCount(position) {
         var infoElement = document.getElementById("Carousel_Info_Box");
-        infoElement.innerHTML = position + "/" + MaxPosition;
+        infoElement.innerHTML = (position) + "/" + (MaxPosition);
     }
 
     return (
@@ -60,21 +79,21 @@ export default function Carousel() {
                     <i className="fas fa-chevron-left" aria-label="Left Chevron Button" role="button" onClick={Previous} onKeyDown={Previous}></i>
                 </div>
                 <div id="Carousel_Control_Right" className="Control_Button" aria-label="Right Chevron Container" role="button" onClick={Next} onKeyDown={Next} >
-                    <i className="fas fa-chevron-left" aria-label="Right Chevron button" role="button" onClick={Next} onKeyDown={Next}></i>
+                    <i className="fas fa-chevron-left"></i>
                 </div>
                 <div className="Carousel_Info">
                     <span id="Carousel_Info_Box">{CarouselIndex}/3</span>
                 </div>
             </div>
             <div id="BA_Content">
-                <div id="ConentContainerRight" className="BeforeAfter DontRotate">
-                    <div id="IntroContentBox" className="DontRotate">
+                <div id="ConentContainerMiddle" className="BeforeAfter DontRotate">
+                    <div id="IntroContentBox" className="DontRotate CenterMargin">
                         <div id="MainContent">
                             <h3 id="MainConentTitleRight">Model</h3>
                             <hr id="hrRight"/>
                             <br/>
                             <div>
-                                <div className="Before"></div>
+                                <div id="CarouselContentImage" style={{backgroundImage: "url(" + firstImage + ")"}}></div>
                                 <br/>
                             </div>
                             <br/>
