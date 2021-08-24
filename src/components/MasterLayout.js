d62268ca5fe58129e06d23df716ec22424a8dc60
James Noonan <warped.demon.prince@hotmail.com>
2021-08-24 20:08:50 +0800

    Update MasterLayout.js

--- src/components/MasterLayout.js
+++ src/components/MasterLayout.js
@@ -28,8 +28,8 @@ export default function MasterLayout({children}) {
                 <meta property="type" content="website"/>
                 <meta property="site_name" content="Quick & Cheap Hair Cuts Perth | Hairdressing | Quick Cut"/>
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"/>
-                <OutboundLink></OutboundLink>
             </Helmet>
+            <OutboundLink></OutboundLink>
             {children}
 
         </div>
