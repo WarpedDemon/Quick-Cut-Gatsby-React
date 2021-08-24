b7b8862ea521e0630f1164926d9bd69d4605a666
James Noonan <warped.demon.prince@hotmail.com>
2021-08-24 20:26:16 +0800

    Fix it matt

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,34 +23,30 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-205628146-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "UA-205628146-1",
-        // Enables Google Optimize Experiment ID
-        experimentId: "UA-205628146-1",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "UA-205628146-1",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "https://www.quickcutwa.com.au/",
-        // defaults to false
-        enableWebVitalsTracking: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-205628146-1", // Google Analytics / GA
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        //gtagConfig: {
+        //  optimize_id: "OPT_CONTAINER_ID",
+        //  anonymize_ip: true,
+        //  cookie_expires: 0,
+        //},
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: false,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+        },
       },
     },
   ],
