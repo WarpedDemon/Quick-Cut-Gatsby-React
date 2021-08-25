e0fd3c9233792034b6e117bb5a9fed19e7c9894e
James Noonan <warped.demon.prince@hotmail.com>
2021-08-25 22:16:21 +0800

    Update SingleParrallaxPage.js

--- src/components/SingleParrallaxPage.js
+++ src/components/SingleParrallaxPage.js
@@ -254,8 +254,8 @@ export default function SingleParrallaxPage() {
       </div>
 
       <div className="PeelParrallax img6" id="img6">
-        <div className="carouselHeader Gallery">
-          <div id="IntroContentBox" className="DontRotate CenterMargin">
+        <div className="carouselHeader Gallery CenterMargin">
+          <div id="IntroContentBox" className="DontRotate">
             <div id="MainContent">
               <h3 id="MainConentTitle">Gallery</h3>
               <hr/>
