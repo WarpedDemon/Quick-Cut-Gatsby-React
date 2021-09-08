2b514e4eba08b6dce0e4d7db76f7f8ea14d1f332
MatthewBryanCC <thepianomanz1011@gmail.com>
2021-09-09 00:11:58 +0800

    Google Analytics Update
    Updated google analytics to only have one listener. This might have been doubling your results.

--- src/components/MasterLayout.js
+++ src/components/MasterLayout.js
@@ -7,7 +7,6 @@ import "../styles/Extras.css"
 import "../styles/animate.css"
 import "../styles/hamburgers.min.css"
 import ToTopButton from './ToTopButton';
-import { OutboundLink } from "gatsby-plugin-google-analytics"
 
 export default function MasterLayout({children}) {
 
@@ -29,7 +28,6 @@ export default function MasterLayout({children}) {
                 <meta property="site_name" content="Quick & Cheap Hair Cuts Perth | Hairdressing | Quick Cut"/>
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"/>
             </Helmet>
-            <OutboundLink></OutboundLink>
             {children}
 
         </div>
