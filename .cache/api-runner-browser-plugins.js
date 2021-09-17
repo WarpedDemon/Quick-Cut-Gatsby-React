2b4ad800aea2d92b258dd9e7f008f123359108db
MatthewBryanCC <thepianomanz1011@gmail.com>
2021-09-17 17:01:17 +0800

    Google G-Tag Removal
    Changing google g-tag from the Gatsby plugin equivalent to a manual script via google tag manager console

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,7 +1,4 @@
 module.exports = [{
       plugin: require('../node_modules/gatsby-plugin-google-tagmanager/gatsby-browser.js'),
       options: {"plugins":[],"id":"GTM-T82H3N4","includeInDevelopment":false,"defaultDataLayer":{"type":"object","value":{"platform":"gatsby"}},"enableWebVitalsTracking":true,"routeChangeEventName":"gatsby-route-change"},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
-      options: {"plugins":[],"trackingIds":["UA-205628146-1"],"pluginConfig":{"head":true,"respectDNT":false,"exclude":[]}},
     }]
