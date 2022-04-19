31683dc533265343be3acdde6991b09d7f7d0109
James Noonan <warped.demon.prince@hotmail.com>
2022-04-19 17:46:17 +0800

    Socials
    Updated Finally

--- src/components/SingleParrallaxPage.js
+++ src/components/SingleParrallaxPage.js
@@ -36,16 +36,20 @@ export default function SingleParrallaxPage() {
               <div>
                 Whitfords:
                 <div>
-                  <Link to="https://www.facebook.com/quickcutWA"><div className="SocialIconImage Facebook"></div></Link>
                   <Link to="https://g.page/qyickcutw?share"><div className="SocialIconImage GMB"></div></Link>
                 </div>
               </div>
               <div>
                 Dianella:
                 <div>
-                  <Link to="https://www.instagram.com/quickcuts_dianella/"><div className="SocialIconImage Insta"></div></Link>
-                  <Link to="https://www.facebook.com/quickcutd"><div className="SocialIconImage Facebook"></div></Link>
                   <Link to="https://g.page/quickcutd?share"><div className="SocialIconImage GMB"></div></Link>
+                </div>
+              </div>
+              <div>
+                Socials:
+                <div>
+                  <Link to="https://www.facebook.com/quickcutperth/"><div className="SocialIconImage Facebook"></div></Link>
+                  <Link to="https://www.instagram.com/quickcutperth/"><div className="SocialIconImage Insta"></div></Link>
                 </div>
               </div>
           </div>
